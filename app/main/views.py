5ee9b248ff77774c7abdfc27d23e7b4617d540a8
“maxwellkimutai” <maxwellkimutai@gmail.com>
2018-10-24 15:39:00 +0300

    add requester template

--- app/main/views.py
+++ app/main/views.py
@@ -32,7 +32,32 @@ def admin():
     progress_tickets_list = Ticket.query.filter_by(status = 'progress').order_by(Ticket.id.desc()).limit(10)
     closed_tickets_list = Ticket.query.filter_by(status = 'closed').order_by(Ticket.id.desc()).limit(10)
 
-    return render_template('admin.html', open_tickets_list = open_tickets_list,progress_tickets_list = progress_tickets_list, closed_tickets_list = closed_tickets_list)
+    labels = ["Open","In progress","Closed"]
+
+    values = []
+
+    tickets = Ticket.query.all()
+
+    open_count = 0
+    progress_count = 0
+    closed_count = 0
+
+    for ticket in tickets:
+        if ticket.status == 'open':
+            open_count += 1
+    values.append(open_count)
+
+    for ticket in tickets:
+        if ticket.status == 'inprogress':
+            progress_count += 1
+    values.append(progress_count)
+
+    for ticket in tickets:
+        if ticket.status == 'closed':
+            closed_count += 1
+    values.append(closed_count)
+
+    return render_template('admin.html', open_tickets_list = open_tickets_list,progress_tickets_list = progress_tickets_list, closed_tickets_list = closed_tickets_list,labels = labels, values = values)
 
 @main.route('/technician', methods=['GET','POST'])
 @login_required
@@ -48,6 +73,11 @@ def technician():
 @main.route('/requester', methods=['GET','POST'])
 @login_required
 def requester():
+
+    open_tickets_list = Ticket.query.filter_by(status = 'open', user_id = current_user.id).order_by(Ticket.id.desc()).limit(10)
+    progress_tickets_list = Ticket.query.filter_by(status = 'inprogress', user_id = current_user.id).order_by(Ticket.id.desc()).limit(10)
+    closed_tickets_list = Ticket.query.filter_by(status = 'closed', user_id = current_user.id).order_by(Ticket.id.desc()).limit(10)
+
     form = TicketForm()
     if form.validate_on_submit():
         print(form.technician.data, form.subject.data, form.severity.data)
@@ -55,7 +85,7 @@ def requester():
         db.session.add(new_ticket)
         db.session.commit()
         return redirect(url_for('.requester'))
-    return render_template('requester.html', form= form)
+    return render_template('requester.html', form= form, open_tickets_list = open_tickets_list, progress_tickets_list = progress_tickets_list, closed_tickets_list = closed_tickets_list)
 
 @main.route('/users', methods = ['GET','POST'])
 def users():
